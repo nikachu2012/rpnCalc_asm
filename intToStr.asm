8c7103cd1e7ca0341192fc3d3c4440e75ba2c884
nikachu2012 <webmaster@nikachu.net>
2024-12-21 18:38:35 +0900

    [fix] スタックを確保、解放を追加し、rbpを書き換えないように変更

--- intToStr.asm
+++ intToStr.asm
@@ -9,20 +9,22 @@ section .text
 ; void intToStr(uint64_t data, char *buf, char *buflen);
 ; 引数　 : rdi, rsi, rdx
 ; 戻り値 : なし
-; 破壊レジスタ: rax, rcx, rdx, r8 (すべてcaller-save)
+; 破壊レジスタ: rax, rcx, rdx, r8, r9 (すべてcaller-save)
 ; ===============================================================
 _intToStr:
     push rbp
     mov rbp, rsp
+    sub rsp, 8*3 ; スタックを24バイト確保
 
     mov rax, rdi ; 演算用にdataをコピー
     mov rcx, 10 ; 割るための定数
+    mov r8, 0 ; ループカウンタの0クリア
 
     ; 最初から0のときの処理
     cmp rax, 0
     jne _intToStr_loop1 ; 呼び出し時に0でないとき
     
-    mov BYTE [rbp], 0x30 ; 呼び出し時に0なら'0'を積んで戻る
+    mov BYTE [rbp + r8], 0x30 ; 呼び出し時に0なら'0'を積んで戻る
     dec rbp
     jmp _intToStr_exit1
 
@@ -34,29 +36,29 @@ _intToStr_loop1:
     mov rdx, 0 ; rdxの0クリア
     idiv rcx ; 10で割って最下位の桁を取り出す
     add rdx, 0x30 ; 余りの数値をASCIIコードにする
-    mov BYTE [rbp], dl ; 左の文字からスタックに積む
+    mov BYTE [rbp + r8], dl ; 左の文字からスタックに積む
 
-    dec rbp ; スタックを1バイト戻す
+    dec r8 ; スタックを1バイト戻す
     jmp _intToStr_loop1
 
 _intToStr_exit1:
     ; スタックからバッファに文字列をコピー
-    mov r8, 0 ; ループカウンタの0クリア
-    inc rbp
+    mov r9, 0 ; ループカウンタ
+    inc r8
 
 _intToStr_loop2:
-    cmp rsp, rbp
-    jl _intToStr_ret ; rsp<rbpになるまでループ
+    cmp r8, 0
+    jg _intToStr_ret ; r8==0なるまでループ
 
-    mov r10b, BYTE [rbp]
-    mov BYTE [rsi + r8], r10b ; rbp+1の値をbufにコピー
+    mov r10b, BYTE [rbp + r8]
+    mov BYTE [rsi + r9], r10b ; rbp+1の値をbufにコピー
 
-    inc rbp
     inc r8
+    inc r9
     jmp _intToStr_loop2
 
 _intToStr_ret:
-    mov BYTE [rsi + r8], 0 ; 末尾にヌル文字を追加
+    mov BYTE [rsi + r9], 0 ; 末尾にヌル文字を追加
     
-    pop rbp
+    leave
     ret
